d75d9a511cc20adbb3a1167c785cef21c5eab3b9
Viacheslav Chuvalskyi <slavchuval@gmail.com>
2021-09-03 22:39:45 +0300

    init main page

---
+++ src/components/organisms/mainPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './styles.module.scss';
+
+function mainPage() {
+
+  return (
+      <div>
+        <h2>Breaking Bad</h2>
+     </div>
+    );
+}
+
+export default mainPage;

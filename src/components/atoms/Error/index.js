c514fe8649805c4b1cdc5410ac3cc32e22fa824f
Viacheslav Chuvalskyi <slavchuval@gmail.com>
2021-09-21 22:28:53 +0300

    Fix mistakes after reviewing

--- src/components/atoms/Error/index.js
+++ src/components/atoms/Error/index.js
@@ -3,7 +3,6 @@ import styles from './styles.module.scss';
 import errorImg from "../../../assets/images/error.gif";
 
 const Error = ({error}) => {
-  console.log(error);
   return (
     <div className={styles.error}>
       <img src={errorImg} alt="error"/>
